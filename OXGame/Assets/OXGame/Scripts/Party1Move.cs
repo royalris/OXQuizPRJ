b81ceff6b1e81db7d68ce44c8086e69702fd7ebc
KimYeongJin <dkdpaxkzl@gmail.com>
2020-05-22 09:43:13 +0900

    12
    1231

---
+++ OXGame/Assets/OXGame/Scripts/Party1Move.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Party1Move : MonoBehaviour
+{
+    public Vector3 waypointO;
+    public Vector3 waypointX;
+    public Vector3 waypointD;
+
+    private Vector3 currPosition;
+    public float speed = 3f;
+
+    private bool ClickO = false;
+    private bool ClickX = false;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currPosition = transform.position;
+        MoveO();
+        MoveX();
+    }
+
+    void MoveO()
+    {
+        if(ClickO == true)
+        {
+            float MoveSpeed = speed * Time.deltaTime;
+            transform.position = Vector3.Lerp(currPosition, waypointO, MoveSpeed);
+        }
+        
+    }
+
+    public void OnClickO()
+    {
+        ClickX = false;
+        ClickO = true;
+    }
+
+    void MoveX()
+    {
+        if(ClickX == true)
+        {
+            float MoveSpeed = speed * Time.deltaTime;
+            transform.position = Vector3.Lerp(currPosition, waypointX, MoveSpeed);
+        }
+    }
+
+    public void OnClickX()
+    {
+        ClickO = false;
+        ClickX = true;
+    }
+}
